3deb21fdc514ecbb7bfa84419e579cec78b2131c
dwen168 <wjian2008@hotmail.com>
2020-02-17 16:33:49 +1100

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/uc?export=download&id=1U0NVvv9zE6pF-T1qJ5FuVRpWxEzCPGB3'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1iCvs6SKsQFR8Axj-rrRatgzNAWtVOmIu'
 export_file_name = 'export.pkl'
 
-classes = ['anger', 'disgust', 'fear', 'happy', 'sad', 'surprise']
+classes = ['Abyssinian', 'British_Shorthairs', 'Dog', 'Horse', 'Persian']
 path = Path(__file__).parent
 
 app = Starlette()
